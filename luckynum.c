44cea2b510545bef81248fe61e5dbebdc03d944a
nafisa <nafisazaman82@gmail.com>
2025-10-15 20:36:50 +0600

    practice array problems

--- luckynum.c
+++ luckynum.c
@@ -4,12 +4,10 @@ int main() {
     int N;
     scanf("%d", &N);
     
-    // Extract the two digits
     int first_digit = N / 10;   // Tens place
     int second_digit = N % 10;  // Units place
     
-    // Check if one digit is divisible by the other
-    // Need to avoid division by zero
+
     int is_lucky = 0;
     
     if (second_digit != 0 && first_digit % second_digit == 0) {
