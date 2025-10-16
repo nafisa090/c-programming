788117350b1f5e9bfba6c8a56e0237d8b4966e20
nafisa <nafisazaman82@gmail.com>
2025-10-16 18:21:58 +0600

    dislike_of_threes prob

--- divisors.c
+++ divisors.c
@@ -4,7 +4,6 @@ int main() {
     int N;
     scanf("%d", &N);
     
-    // Find all divisors from 1 to N
     for (int i = 1; i <= N; i++) {
         if (N % i == 0) {
             printf("%d\n", i);
